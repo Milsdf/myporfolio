e24c27edacc62665d53a4b5240d9786f121e910f
Milsdf <milanalatova@gmail.com>
2024-09-23 10:25:52 +0300

    add files to GutHub

---
+++ myportfolio/src/components/header/Header.js
@@ -0,0 +1,38 @@
+import "./style.css";
+import d1 from "../../img/d/d1.png"
+import d2 from "../../img/d/d2.png"
+import d3 from "../../img/d/d3.png"
+import d5 from "../../img/d/d4.png"
+import d4 from "../../img/d/d5.png"
+import d6 from "../../img/d/d6.png"
+import d7 from "../../img/d/d7.png"
+import d8 from "../../img/d/d8.png"
+import d9 from "../../img/d/d9.png"
+import d10 from "../../img/d/d10.png"
+import d11 from "../../img/d/d11.png"
+
+
+
+
+const Header = () => {
+    return (
+		<header className="header">
+			<div className="header__wrapper">
+				
+			</div>
+			<img src={d1} alt="" className="project__img" />\
+			<img src={d2} alt="" className="project__img" />
+			<img src={d3} alt="" className="project__img" />
+			<img src={d4} alt="" className="project__img" />
+			<img src={d5} alt="" className="project__img" />
+			<img src={d6} alt="" className="project__img" />
+			<img src={d7} alt="" className="project__img" />
+			<img src={d8} alt="" className="project__img" />
+			<img src={d9} alt="" className="project__img" />
+			<img src={d10} alt="" className="project__img" />
+			<img src={d11} alt="" className="project__img" />
+		</header>
+	);
+}
+
+export default Header;
---
+++ myportfolio/src/helpers/projectsList.js
@@ -0,0 +1,98 @@
+import project01 from "./../img/projects/forecast.png";
+import project01Big from "./../img/projects/forecast.png";
+
+import project02 from "./../img/projects/guitar.png";
+import project02Big from "./../img/projects/guitar.png";
+
+import project03 from "./../img/projects/scrol-site.png";
+import project03Big from "./../img/projects/scrol-site.png";
+
+ import project04 from "./../img/projects/todolist.png";
+ import project04Big from "./../img/projects/todolist.png";
+
+import project05 from "./../img/projects/bootst.png";
+import project05Big from "./../img/projects/bootst.png";
+
+import project06 from "./../img/projects/quiz.png";
+import project06Big from "./../img/projects/quiz.png";
+
+import project07 from "./../img/projects/clock.png";
+import project07Big from "./../img/projects/clock.png";
+
+import project08 from "./../img/projects/transleter.png";
+import project08Big from "./../img/projects/transleter.png";
+
+import project09 from "./../img/projects/text.png";
+import project09Big from "./../img/projects/text.png";
+
+
+const projects = [
+	{
+		title: 'Bootstrat project',
+		img: project05,
+		imgBig: project05Big,
+		skills: 'Bootstrat',
+		gitHubLink: 'https://milsdf.github.io/bootsrap-project/',
+	},
+	{
+		title: 'TODO LIST',
+		img: project04,
+		imgBig: project04Big,
+		skills: 'HTML CSS JS',
+		gitHubLink: 'https://milsdf.github.io/toDoList/',
+	},
+	{
+		title: 'Translator',
+		img: project08,
+		imgBig: project08Big,
+		skills: 'HTML CSS JS',
+		gitHubLink: 'https://milsdf.github.io/translator/',  
+	},
+	{
+		title: 'Clock',
+		img: project07,
+		imgBig: project07Big,
+		skills: 'HTML CS JS',
+	gitHubLink: 'https://milsdf.github.io/clock/',
+	},
+	{
+		title: 'Weather forecast',
+		skills: 'Api, JS',
+		img: project01,
+		imgBig: project01Big,
+		gitHubLink: ' https://milsdf.github.io/weather-forecast/'
+	},
+	
+	{
+		title: 'Speech to text',
+		skills: 'HTML CSS, JS',
+		img: project09,
+		imgBig: project09Big,
+		gitHubLink: 'https://milsdf.github.io/text/ '
+	},
+	{
+		title: 'Guitar store',
+		img: project02,
+		imgBig: project02Big,
+		skills: 'JS',
+		gitHubLink: 'https://milsdf.github.io/gitar-store/',
+	},
+	{
+		title: 'Parallax-skrolling website',
+		img: project03,
+		imgBig: project03Big,
+		skills: 'css',
+		gitHubLink: 'https://milsdf.github.io/parallax-scrolling/',
+	},
+	
+	
+	{
+		title: 'Quiz Game',
+		img: project06,
+		imgBig: project06Big,
+		skills: 'HTML CS JS',
+	gitHubLink: 'https://milsdf.github.io/quiz/',
+	},
+];
+
+export {projects}
